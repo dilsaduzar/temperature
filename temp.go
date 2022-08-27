165676a80321404a77b5128ef57c1e95a8e45964
dilsad <uzardilsad@gmail.com>
2022-08-27 16:26:00 +0300

    revised temp.go file

--- temp.go
+++ temp.go
@@ -4,16 +4,16 @@ import "fmt"
 
 func Conv(temp float64, symbol string) (float64, error) {
 	if symbol == "C" || symbol == "c" {
-		return CtoF(temp), nil
+		return CtoF(temp)
 	} else if symbol == "F" || symbol == "f" {
-		return FtoC(temp), nil
+		return FtoC(temp)
 	}
 	return 0, fmt.Errorf("unrecognized symbol: %q, want: F, f, C or c\n", symbol)
 }
 
-func FtoC(a float64) float64 {
-	return (a - 32) * 5 / 9
+func FtoC(a float64) (float64, error) {
+	return (a - 32) * 5 / 9, nil
 }
-func CtoF(b float64) float64 {
-	return (b * 1.8) + 32
+func CtoF(b float64) (float64, error) {
+	return (b * 1.8) + 32, nil
 }
