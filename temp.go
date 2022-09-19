4b9a0f5c7eaead84f9c1573e7fc2ebe4b334489d
dilsad <uzardilsad@gmail.com>
2022-09-19 17:06:10 +0300

    revised file

--- temp.go
+++ temp.go
@@ -2,18 +2,18 @@ package temperature
 
 import "fmt"
 
-func Conv(temp float64, symbol string) (float64, error) {
+func Conv(temp float64, symbol string) (float64, string, error) {
 	if symbol == "C" || symbol == "c" {
 		return CtoF(temp)
 	} else if symbol == "F" || symbol == "f" {
 		return FtoC(temp)
 	}
-	return 0, fmt.Errorf("unrecognized symbol: %q, want: F, f, C or c\n", symbol)
+	return 0, "", fmt.Errorf("unrecognized symbol: %q, want: F, f, C or c\n", symbol)
 }
 
-func FtoC(a float64) (float64, error) {
-	return (a - 32) * 5 / 9, nil
+func FtoC(a float64) (float64, string, error) {
+	return (a - 32) * 5 / 9, "C", nil
 }
-func CtoF(b float64) (float64, error) {
-	return (b * 1.8) + 32, nil
+func CtoF(b float64) (float64, string, error) {
+	return (b * 1.8) + 32, "F", nil
 }
